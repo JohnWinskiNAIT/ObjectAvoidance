fbda35afa93e9a853631102312dda938aa4929ad
John Winski <JWINSKI@nait.ca>
2023-10-17 15:16:39 -0600

    Added 45 degree walls

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float movementSpeed = 3.0f, distance = 3.0f;
+    [SerializeField] float movementSpeed = 3.0f, forwardDist = 1.0f, sideDist = 3.0f;
     
+    bool isLeft, isRight;
+
+    RaycastHit hit;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +19,52 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Physics.Raycast(transform.position, transform.forward, distance))
+        if (Physics.BoxCast(transform.position, new Vector3(0.5f, 0.5f, 0.5f), transform.forward, out hit, Quaternion.identity, forwardDist))
         {
-            transform.Rotate(Vector3.up, 90);
+            // Rotate to the right
+            //transform.Rotate(Vector3.up, 90);
+
+            // Rotate a random direction
+            //if (Random.Range(1, 3) == 1)
+            //{
+            //    transform.Rotate(Vector3.up, 90);
+            //}
+            //else
+            //{
+            //    transform.Rotate(Vector3.up, -90);
+            //}
+            transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, -hit.normal, 1, 1));
+
+
+
+            // Rotate based on what is to the sides
+            isLeft = Physics.Raycast(transform.position, -transform.right, sideDist);
+            isRight = Physics.Raycast(transform.position, transform.right, sideDist);
+
+            if (isLeft && isRight)
+            {
+                transform.Rotate(Vector3.up, 180);
+            }
+            else if (isLeft && !isRight)
+            {
+                transform.Rotate(Vector3.up, 90);
+            }
+            else if (!isLeft && isRight)
+            {
+                transform.Rotate(Vector3.up, -90);
+            }
+            else
+            {
+                if (Random.Range(1, 3) == 1)
+                {
+                    transform.Rotate(Vector3.up, 90);
+                }
+                else
+                {
+                    transform.Rotate(Vector3.up, -90);
+                }
+            }
+
         }
         
         
