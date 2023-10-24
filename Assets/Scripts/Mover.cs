7d9c1f6978c75a6a367b6cb91f50a442b33efdf1
John Winski <JWINSKI@nait.ca>
2023-10-24 15:20:34 -0600

    Adjusted area

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -68,11 +68,11 @@ public class Mover : MonoBehaviour
             if (hit.transform.gameObject.tag == "Wall")
             {
 
-                transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, -hit.normal, 1, 1));
-
                 // Rotate based on what is to the sides
                 isLeft = Physics.Raycast(transform.position, -transform.right, sideDist);
                 isRight = Physics.Raycast(transform.position, transform.right, sideDist);
+
+                transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, -hit.normal, 1, 1));
 
                 if (isLeft && isRight)
                 {
